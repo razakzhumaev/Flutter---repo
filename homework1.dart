026806dc0fffe9c89508c9eac39a975495dbaac7
razakzhumaev <146742354+razakzhumaev@users.noreply.github.com>
2023-10-05 23:23:45 +0600

    Add files via upload
    homework

---
+++ homework1.dart
@@ -0,0 +1,48 @@
+void main(List<String> args) {
+  num a = 18;
+  num b = 9.4;
+  num c = 23.3;
+  num d = 100;
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(a + b + c + d);
+  int j = 34;
+  int l = 78;
+  int f = 32;
+  int e = 48;
+  print(j);
+  print(l);
+  print(f);
+  print(e);
+
+  double n = 43;
+  double p = 66;
+  double m = 11.4;
+  double g = 81.977;
+  print(n);
+  print(p);
+  print(m);
+  print(g);
+
+  String array = "Welcome to Flutter!";
+  print(array);
+  String array1 = "My name is Razak";
+  print(array1);
+  String array2 = "I am 18 years old";
+  print(array2);
+  String array3 = "I live in Kyrgyzstan";
+  print(array3);
+
+  bool isStudent = true;
+  bool isTeacher = false;
+  print(isStudent);
+  print(isTeacher);
+
+  List info = [2, 4, 5, 3, "razak", "razak 18 y.o", 55.949, 23.3, false];
+  print(info);
+
+  List<String> question = ["How old are you?"];
+  print(question);
+}
